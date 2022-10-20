5030faab59b39f77705e690b8ea5aba26d742780
Ruiqi Zhong <ruiqi-zhong@rainbowquartz.Berkeley.EDU>
2022-10-20 16:11:39 -0700

    inference code working

--- seq2seq.py
+++ seq2seq.py
@@ -36,7 +36,7 @@ def fit_bit(model):
             parameters.requires_grad = False
 
 
-print('Using %d number of GPUs: ' % torch.cuda.device_count())
+print('Using %d GPUs.' % torch.cuda.device_count())
 mount_dir = 'mount/'
 
 if __name__ == '__main__':
@@ -45,8 +45,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--model_init_name', type=str, default=None)
     parser.add_argument('--model_init_path', type=str, default=None)
-    parser.add_argument('--data_name', type=str)
-    parser.add_argument('--training_run_name', type=str)
+    parser.add_argument('--data_name', type=str, default='data')
+    parser.add_argument('--training_run_name', type=str, default='debug')
     parser.add_argument('--warmup_steps', type=int, default=1000)
     parser.add_argument('--max_steps', type=int, default=3000)
     parser.add_argument('--train_batch_size', type=int, default=32)
@@ -54,7 +54,7 @@ if __name__ == '__main__':
     parser.add_argument('--gradient_accumulation_steps', type=int, default=1)
     parser.add_argument('--save_steps', type=int, default=1000)
     parser.add_argument('--temperature', type=float, default=0.8)
-    paraser.add_argument('--n_samples', type=int, default=8)
+    parser.add_argument('--n_samples', type=int, default=8)
 
     args, unknown = parser.parse_known_args()
     
@@ -71,7 +71,8 @@ if __name__ == '__main__':
     gradient_accumulation_steps = args.gradient_accumulation_steps
     save_steps = args.save_steps
 
-    tokenizer_path = mount_dir + "models/t5-small-cp_tokenizer"
+    # tokenizer_path = mount_dir + "models/t5-small-cp_tokenizer"
+    tokenizer_path = 't5-small'
 
     model = AutoModelForSeq2SeqLM.from_pretrained(model_init_path)
 
@@ -96,7 +97,7 @@ if __name__ == '__main__':
     eval_dataset = PromptCompletionDataset(test, model_tokenizer)
 
     # get a bunch of training arguments
-    args = get_seq2seq_training_args(
+    training_args = get_seq2seq_training_args(
         training_run_name=training_run_name,
         warmup_steps=warmup_steps, max_steps=max_steps,
         train_batch_size=train_batch_size, eval_batch_size=eval_batch_size, 
@@ -108,7 +109,7 @@ if __name__ == '__main__':
     # training loop
     trainer = Seq2SeqTrainer(
         model=model,
-        args=args,
+        args=training_args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
         data_collator=data_collator,
@@ -117,9 +118,5 @@ if __name__ == '__main__':
     trainer.train()
 
     test_prompts = [d['prompt'] for d in test]
-    sampled_results = sample_batched(model_tokenizer, test_prompts, args.temperature, args.n_samples)
-
-
-def sample_batched(
-    model_tokenizer, prompts, temperature=0.8, n=8, bsize=8, 
-    max_source_length=1024, max_target_length=512, save_score_tok_idx=None, verbose=False, stop_strs=None)
+    sampled_results = sample_batched(model_tokenizer, test_prompts, temperature=args.temperature, n=args.n_sample, bsize=args.eval_batch_size)
+    print(sampled_results)
